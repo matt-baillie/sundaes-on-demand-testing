72fc4ecb64b9a43523c508d94e393f8caa4533f3
Matthew Baillie <mbaillie93@gmail.com>
2022-11-06 16:43:18 -0800

    OrderDetails context provider created

---
+++ sundaes-on-demand/src/contexts/OrderDetails.jsx
@@ -0,0 +1,56 @@
+import { createContext, useContext, useState } from "react";
+import { pricePerItem } from "../constants";
+
+const OrderDetails = createContext();
+
+// Create custom hook that checks whether we are in a Provider
+// If it is, return context value
+
+export function useOrderDetails() {
+  const contextValue = useContext(OrderDetails);
+
+  if (!contextValue) {
+    throw new Error(
+      "userOrderDetails must be called from within an OrderDetailsProvider"
+    );
+  }
+
+  return contextValue;
+}
+
+export function OrderDetailsProvider(props) {
+  const [optionCounts, setOptionCounts] = useState({
+    scoops: {},
+    topping: {},
+  });
+  function updateItemCount(itemName, newItemCount, optionType) {
+    // make a copy of existing state
+    const newOptionCounts = { ...optionCounts };
+
+    // update the copy with the new info
+    newOptionCounts[optionType][itemName] = newItemCount;
+
+    // update the state with the updated copy
+    setOptionCounts(newOptionCounts);
+  }
+
+  function resetOrder() {
+    setOptionCounts({ scoops: 0, toppings: 0 });
+  }
+
+  // utility function to derive totals from optionCounts state value
+  function calculateTotal(optionType) {
+    // get array of counts for the option types []
+
+    const countsArray = Object.values(optionCounts[optionType]);
+
+    //total the values in the array of counts for the number of items
+    const totalCount = countsArray.reduce((total, value) => total + value, 0);
+
+    // multiply the total number of items by the price for this item type
+    return totalCount * pricePerItem[optionType];
+  }
+
+  const value = { optionCounts, updateItemCount, resetOrder };
+  return <OrderDetails.Provider value={value} {...props} />;
+}
---
+++ sundaes-on-demand/src/pages/entry/tests/totalUpdates.test.jsx
@@ -0,0 +1,29 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
+import Options from "../Options";
+
+test("Update scoop subtotal when scoops change", async () => {
+  const user = userEvent.setup();
+  render(<Options optionType="scoops" />);
+
+  // flow: initial subTotal $0.00
+  const scoopsSubtotal = screen.getByText("Scoops total: $", { exact: false });
+  expect(scoopsSubtotal).toHaveTextContent("0.00");
+
+  // flow: add 1 option(vanilla) and check subtotal
+  const vanillaInput = await screen.findByRole("spinbutton", {
+    name: "Vanilla",
+  });
+  await user.clear(vanillaInput);
+  await user.type(vanillaInput, "1");
+
+  expect(scoopsSubtotal).toHaveTextContent("2.00");
+  // flow: update chocolate scoops to 2 and check subtotal
+  const chocolateInput = await screen.findByRole("spinbutton", {
+    name: "Chocolate",
+  });
+  await user.clear(chocolateInput);
+  await user.type(chocolateInput, "2");
+  expect(scoopsSubtotal).toHaveTextContent("6.00");
+});
